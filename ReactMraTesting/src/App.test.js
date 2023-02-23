b5053637a1f93ed51ada99c1dc304d6d54cb24e3
SaikumarK1-hexaware <114009121+SaikumarK1-hexaware@users.noreply.github.com>
2023-02-23 15:18:38 +0530

    Removed Comments

--- ReactMraTesting/src/App.test.js
+++ ReactMraTesting/src/App.test.js
@@ -5,7 +5,6 @@ import {
     BrowserRouter as Router,
   } from "react-router-dom";
 
-//hello
   
 test('should render App Component and match the project name', async () => {
     render(<Router ><App /></Router>)
